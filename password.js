758806c6f361c63a29375cb5e59a75f545bb9fbf
Adam Hunter <hunteradam13@gmail.com>
2021-09-28 16:43:44 -0600

    updating passwords

--- password.js
+++ password.js
@@ -1,13 +1,29 @@
-console.log("Welcome to the password validator tool!");
-console.log("Enter your password for processing")
+// console.log("Welcome to the password validator tool!");
+// console.log("Enter your password for processing")
 
-function validator(password){
+// function validator(password){
+//     if(password.length >= 10){
+//         console.log("You passed")
+//     } else {
+//         console.log("Try again")
+//     }
+// }
+
+// validator("thisPasswordShouldBeLongEnough")
+
+console.log('Welcome to the Password Validator Tool!')
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question('What password are you trying to validate?', password => {
+    String(password);
     if(password.length >= 10){
-        console.log("You passed")
+        console.log('Congratulations, your password is approved!');
+        readline.close();
     } else {
-        console.log("Try again")
+        console.log('Sorry, your password failed.')
     }
-}
-
-validator("thisPasswordShouldBeLongEnough")
-
+})
